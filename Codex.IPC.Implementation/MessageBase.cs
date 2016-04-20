b1081fa5282015ca34ade3ed364bb046c2c509dd
Code Codex <tanveer_ahmed@outlook.com>
2016-04-20 17:39:53 -0400

    Added GZIP compression to reduce the data sent over the wire.

--- Codex.IPC.Implementation/MessageBase.cs
+++ Codex.IPC.Implementation/MessageBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -9,50 +10,98 @@ using System.Threading.Tasks;
 
 namespace Codex.IPC.Implementation
 {
-    /// <summary>
-    /// Base class fo the messages that are exchanged between the client and the server.
-    /// </summary>
-    [DataContract]
-    public abstract class MessageBase
-    {
-        /// <summary>
-        /// Body of the message, this contains serialized structures.
-        /// </summary>
-        [DataMember]
-        public Byte[] Body { get; set; }
+   /// <summary>
+   /// Base class fo the messages that are exchanged between the client and the server.
+   /// </summary>
+   [DataContract]
+   public abstract class MessageBase
+   {
+      /// <summary>
+      /// Body of the message, this contains serialized structures.
+      /// </summary>
+      [DataMember]
+      public Byte[] Body { get; set; }
 
-        /// <summary>
-        /// Sets the body of the Message.
-        /// </summary>
-        /// <typeparam name="T">Type of the data</typeparam>
-        /// <param name="data">Body content</param>
-        /// <remarks>
-        /// The data should be a value type and should be serializable.
-        /// </remarks>
-        public void SetBody<T>(T data)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(stream, data);
-                Body = stream.ToArray();
-            }
-        }
+      /// <summary>
+      /// Sets the body of the Message.
+      /// </summary>
+      /// <typeparam name="T">Type of the data</typeparam>
+      /// <param name="data">Body content</param>
+      /// <remarks>
+      /// The data should be a value type and should be serializable.
+      /// </remarks>
+      public void SetBody<T>(T data)
+      {
+         using (MemoryStream stream = new MemoryStream())
+         {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, data);
+            Body = Compress(stream.ToArray());
+         }
+      }
 
-        /// <summary>
-        /// Gets the body of the Message.
-        /// </summary>
-        /// <typeparam name="T">Type of the data</typeparam>
-        /// <remarks>
-        /// The data should be a value type and should be serializable.
-        /// </remarks>
-        public T GetBody<T>()
-        {
-            using (MemoryStream stream = new MemoryStream(Body))
+      /// <summary>
+      /// Gets the body of the Message.
+      /// </summary>
+      /// <typeparam name="T">Type of the data</typeparam>
+      /// <remarks>
+      /// The data should be a value type and should be serializable.
+      /// </remarks>
+      public T GetBody<T>()
+      {
+         using (MemoryStream stream = new MemoryStream(Decompress(Body)))
+         {
+            BinaryFormatter formatter = new BinaryFormatter();
+            return (T)formatter.Deserialize(stream);
+         }
+      }
+
+
+      /// <summary>
+      /// Compresses byte array to new byte array.
+      /// </summary>
+      public static byte[] Compress(byte[] raw)
+      {
+         using (MemoryStream memory = new MemoryStream())
+         {
+            using (GZipStream gzip = new GZipStream(memory,
+           CompressionMode.Compress, true))
             {
-                BinaryFormatter formatter = new BinaryFormatter();
-                return (T)formatter.Deserialize(stream);
+               gzip.Write(raw, 0, raw.Length);
             }
-        }
-    }
+            return memory.ToArray();
+         }
+      }
+
+
+
+      /// <summary>
+      /// Decompress a byte array into object
+      /// </summary>
+      /// <param name="gzip"></param>
+      /// <returns></returns>
+      public static byte[] Decompress(byte[] gzip)
+      {
+         using (GZipStream stream = new GZipStream(new MemoryStream(gzip),
+                          CompressionMode.Decompress))
+         {
+            const int size = 4096;
+            byte[] buffer = new byte[size];
+            using (MemoryStream memory = new MemoryStream())
+            {
+               int count = 0;
+               do
+               {
+                  count = stream.Read(buffer, 0, size);
+                  if (count > 0)
+                  {
+                     memory.Write(buffer, 0, count);
+                  }
+               }
+               while (count > 0);
+               return memory.ToArray();
+            }
+         }
+      }
+   }
 }
